09ab83e70ab870ec2f60b90ac8d113e70791adc1
YoeJoe16 <gonzalezyoel90@gmail.com>
2025-10-23 22:54:20 -0600

    Primer repositorio

---
+++ src/main/java/Cine/Movie.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Cine;
+
+/**
+ *
+ * @author Yoel Gonzalez
+ */
+public class Movie {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
